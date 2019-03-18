fab01ea21554fe56ed899ba406e9f228e1b7460d
Stately World <statelyworld@gmail.com>
2019-03-18 21:00:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo "Welcome to Stately World!";?>
+<?php 
+echo "Welcome to Stately World!";
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo "<br>".$x;
+?>
