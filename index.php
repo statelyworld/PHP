c0949cbedb03bfb585e2994cf7f22d6128911457
Stately World <statelyworld@gmail.com>
2019-03-18 21:06:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,5 +12,16 @@ lines
 
 // You can also use comments to leave out parts of a code line
 $x = 5 /* + 15 */ + 5;
-echo "<br>".$x;
+echo "<br>".$x."<br>";
+
+//PHP Case Sensitivity
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+
+//PHP Case Sensitivity
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
