1228de49f646d1d7aa1a6dd1bd5fcfafc82deb80
Stately World <statelyworld@gmail.com>
2019-04-07 07:53:06 +0530

    Create Example2.php

---
+++ PHP Arrays/Example2.php
@@ -0,0 +1,21 @@
+<?php 
+$numbers=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25);
+//print_r($numbers);
+foreach ($numbers as $value) {
+	echo "value is $value</br>";
+}
+
+$salaries  = array(
+	'mohammad' => 2000,
+	'mohammad1' => 2000,
+	'mohammad2' => 2000,
+	'mohammad3' => 2000,
+	'mohammad4' => 2000,
+	'mohammad5' => 2000,
+	'mohammad6' => 2000,
+	 );
+
+foreach ($salaries as $key => $value) {
+	echo "string $key $value</br>";
+}
+ ?>
